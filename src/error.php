f484257800b16b6641ef07760f0e98de954388d8
Ruben Philipp <me@rubenphilipp.com>
2025-02-20 23:16:16 +0100

    added error files and htacces for modrewrite

---
+++ src/error.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html>
+
+    <head>
+        <meta charset="utf-8">
+        <title>dear lover</title>
+        <meta name="description" content="dear lover">
+        <meta name="author" content="Greta Gottschalk, Ruben Philipp">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+
+        <script src="js/jquery-3.7.1.min.js"></script>
+        <script src="js/jquery.lazy/jquery.lazy.min.js"></script>
+        <script type="text/javascript" src="js/jquery.lazy/plugins/jquery.lazy.av.min.js"></script>
+        <script src="js/main.js"></script>
+    </head>
+
+    <body>
+
+        <div class="main">
+            
+            <header>
+                <h1 style="text-align: right;"><a style="cursor: w-resize;" href="index.php">dear lover</a></h1>
+            </header>
+            
+            <main>
+
+                <h2><u>error</u></h2>
+
+                <p>the page has not been found. </p>
+                
+            </main>
+
+            <footer>
+                <!-- <a href="imprint.php">imprint</a> -->
+            </footer>
+
+        </div>
+        
+    </body>
+
+</html>
