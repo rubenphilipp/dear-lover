42034acfff67059ccdc9dae57340aef69503cdb5
Ruben Philipp <me@rubenphilipp.com>
2025-02-20 01:14:05 +0100

    proper blueprint with functionality

---
+++ src/globals.php
@@ -0,0 +1,16 @@
+<?php
+
+////////////////////////////////////////////////////////////////////////////////
+// GLOBAL CONSTANTS
+const DEBUG = false;
+// the directory containing the media files
+const MEDIA_DIR = "media/"; // needs to end with a "/"
+// the directory containing the meta-data to the media files
+const DATA_DIR = "data/"; // needs to end with a "/"
+// the suffix for the meta-data files
+const DATA_SUFFIX = "*.yaml";
+// the HTTP-root
+const HTTP_ROOT = "/";
+
+
+?>
--- src/index.php
+++ src/index.php
@@ -11,17 +11,103 @@
 
         <?php
 
-        $files = glob('data/*.txt');
+        ////////////////////////////////////////
+        // INCLUDES
+        // -------------------------------------
+        // global definitions
+        include 'globals.php';
+        ////////////////////////////////////////
 
-        foreach($files as $file) {
-            echo "<br>";
+        ////////////////////////////////////////
+        // FUNCTIONS
 
-            $contents = file_get_contents($file);
-            $parsed = yaml_parse($contents);
-            
-            echo $file . "<br>";
+        // This function takes an array with file names
+        // as its first argument (e.g. retrieved via glob)
+        // and returns new array with the data retrieved from
+        // the files, assuming that the meta-data-YAML contains
+        // the following mandatory fields:
+        // - date: the date of the respective letter (formatted:
+        //   YYYY-MM-DD HH-MM)
+        // - file: the video file (relative to MEDIA_DIR)
+        // - from: the sender name (either "greta" or "ruben")
+        // - to: the recipient name (either "greta" or "ruben")
+        // and OPTIONALLY:
+        // - comment: a string as a comment
+        // - still: path to a still image (relative to MEDIA_DIR)
+        //
+        // ADDITIONALLY:
+        // - the resulting array also includes a "timestamp"
+        //   field, which is a UNIX timestamp generated from the
+        //   date value
+        function parseMetaFiles($fileArray) {
+            $res = array();
+            foreach($fileArray as $key => $f) {
+                $res[$key] = parseMetaFile($f);
+            }
+            return $res;
         }
 
+        function parseMetaFile($file) {
+            $res = array();
+            
+            $contents = file_get_contents($file);
+            $parsed = yaml_parse($contents);
+
+            // date
+            $res["date"] = $parsed["date"];
+            // file
+            $res["file"] = $parsed["file"];
+            $res["from"] = $parsed["from"];
+            $res["to"] = $parsed["to"];
+            if(isset($parsed["comment"])) {
+                $res["comment"] = $parsed["comment"];
+            }
+            else {
+                $res["comment"] = null;
+            }
+            $res["date"] = $parsed["date"];
+            // timestamp
+            $res["timestamp"] = tsFromStr($parsed["date"]);
+            // still
+            if(isset($parsed["still"])) {
+                $res["still"] = $parsed["still"];
+            }
+            else {
+                $res["still"] = null;
+            }
+
+            return $res;
+        }
+
+        function tsFromStr($s) {
+            $dP = date_parse_from_format("Y-m-d H:i:s", $s);
+            $ts = mktime($dP["hour"], $dP["minute"], $dP["second"], $dP["month"], $dP["day"], $dP["year"]);
+            return $ts;
+        }
+        
+
+        ////////////////////////////////////////
+
+        // list all meta-data files:
+        $files = glob(DATA_DIR . DATA_SUFFIX);
+
+        $letters = parseMetaFiles($files);
+
+        // sort letters
+        uasort($letters,
+               function($a, $b) {
+                   return ($a["timestamp"] > $b["timestamp"]) ? -1 : 1;
+        });
+
+        ////////////////////////////////////////
+
+        foreach($letters as $letter) {
+            echo $letter["file"];
+            echo "<br>";
+            echo $letter["date"] . " " . $letter["timestamp"];
+            echo "<br>";
+        }
+        
         ?>
 
     </body>
