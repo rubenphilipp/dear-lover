be5acb8c3a9787a927f0d6776339582ab0836e75
Ruben Philipp <me@rubenphilipp.com>
2025-02-20 23:04:40 +0100

    changed data structure

--- src/globals.php
+++ src/globals.php
@@ -4,9 +4,9 @@
 // GLOBAL CONSTANTS
 const DEBUG = false;
 // the directory containing the media files
-const MEDIA_DIR = "media/"; // needs to end with a "/"
+const MEDIA_DIR = "letters/media/"; // needs to end with a "/"
 // the directory containing the meta-data to the media files
-const DATA_DIR = "data/"; // needs to end with a "/"
+const DATA_DIR = "letters/data/"; // needs to end with a "/"
 // the suffix for the meta-data files
 const DATA_SUFFIX = "*.yaml";
 // the HTTP-root
